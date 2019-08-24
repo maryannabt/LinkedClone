c73614404c9461c6f9ebe416a3a4ff0dee07a88a
netzcat <netzzzcat@gmail.com>
2019-08-24 17:24:09 +0300

    Added a component Post_Profile which shows the info of the author of the post (in the list of posts).

---
+++ client/src/components/MainRail/Post.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import Loader from "../../img/Loader";
+import Profile from "./Post_Profile";
+
+class Post extends Component {
+  state = { doneLoading: false };
+
+  componentDidMount() {
+    this.setState({ doneLoading: true });
+  }
+
+  render() {
+    const { postAuthUser, updatedAt } = this.props;
+
+    if (!this.state.doneLoading) {
+      return <Loader />;
+    } else {
+      return (
+        <PostDiv>
+          <Profile {...postAuthUser} timeOfPost={updatedAt} />
+        </PostDiv>
+      );
+    }
+  }
+}
+
+export default Post;
+
+// CSS
+const PostDiv = styled.div`
+  width: 54.8rem;
+  background-color: #ffffff;
+  box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.15), 0 2px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 1rem;
+
+  @media only screen and (max-width: 580px) {
+    width: 99%;
+  }
+`;
